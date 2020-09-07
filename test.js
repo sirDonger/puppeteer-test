be7aa13d06a9c7d661013e7f6eaee21c6a482bc0
Mykola Sirko <ssrko98@gmail.com>
2020-09-07 12:42:48 +0300

    fixed errors

--- test.js
+++ test.js
@@ -38,17 +38,78 @@ const sender = async (email, subject) => {
 app.get('/send', async function main() {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+
+    const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+    await page.setUserAgent(userAgent);
+
+    if (/HeadlessChrome/.test(navigator.userAgent)) {
+        throw new Error(`invalid data`);
+    }
+
+    if (navigator.webdriver) {
+        throw new Error(`invalid data`);
+    }
+
+    await page.evaluateOnNewDocument(() => {
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => false,
+        });
+    });
+
+    if (!window.chrome) {
+        throw new Error(`invalid data`);
+    }
+
+    await page.evaluateOnNewDocument(() => {
+        window.navigator.chrome = {
+            runtime: {},
+        };
+    });
+
+    if (!window.chrome || !window.chrome.runtime) {
+        throw new Error(`invalid data`);
+    }
+
+    await page.evaluateOnNewDocument(() => {
+        const originalQuery = window.navigator.permissions.query;
+        return window.navigator.permissions.query = (parameters) => (
+            parameters.name === 'notifications' ?
+                Promise.resolve({state: Notification.permission}) :
+                originalQuery(parameters)
+        );
+    });
+
+
+    await page.evaluateOnNewDocument(() => {
+        Object.defineProperty(navigator, 'plugins', {
+            get: () => [1, 2, 3, 4, 5],
+        });
+    });
+
+
+    await page.evaluateOnNewDocument(() => {
+        Object.defineProperty(navigator, 'languages', {
+            get: () => ['en-US', 'en'],
+        });
+    });
+
     await page.goto(`${url}`, {waitUntil: ['load', 'networkidle2']});
     await page.type('#emailOrNickname', `mykola.sirko@workgmail.com`);
     await page.type('#password', `Starapps123@`);
-    const element = await page.$('[class="signInButton-3250695435');
+
+    const element = await page.$('[class="signInButton-3250695435 button-1997310527 button__futurePrimary-3327793552 button__medium-1066667140"]');
     await element.click();
+
     await page.goto(`${url}`, {waitUntil: 'networkidle0'});
     await page.type('#ProfileName', `Mykola`);
+
     const checkBox = await page.$('[for="sendFeatureNotificationEmail');
     await checkBox.click();
+
     const submitChanges = await page.$('[class="button-task"');
     await submitChanges.click();
+
     await page.reload({waitUntil: ["networkidle0", "domcontentloaded"]});
     await page.screenshot({path: 'screenshot.png'});
 
